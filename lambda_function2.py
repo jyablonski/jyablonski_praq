7f3c8f9b80a66881d7c8124e9c14347eca1726f4
jyablonski9 <jyablonski9@gmail.com>
2021-12-07 18:44:09 -0600

    test lint

--- lambda_function2.py
+++ lambda_function2.py
@@ -41,9 +41,8 @@ def get_transactions():
 d = {"col1": [1, 2], "col2": [3, 4]}
 df = pd.DataFrame(data=d)
 df
-
-
-def sql_connection():
+# test 11111
+def sql_connection():#11111
     try:
         connection = create_engine(
             "mysql+pymysql://"
