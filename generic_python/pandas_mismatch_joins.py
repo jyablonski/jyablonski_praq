ff9ffa21adf5790b2e5cc8185313a6fc5afa4f1a
jyablonski9 <jyablonski9@gmail.com>
2023-08-29 17:22:41 -0500

    ad

--- generic_python/pandas_mismatch_joins.py
+++ generic_python/pandas_mismatch_joins.py
@@ -1,10 +1,24 @@
+from datetime import datetime, timedelta
+
 import pandas as pd
 
 # existing data
 df1 = pd.DataFrame(data={"id": [1, 2, 3]})
 
 # values we want to add in if not already present
-df2 = pd.DataFrame(data={"id": [2, 3, 4], "sales": [100, 200, 300]})
+df2 = pd.DataFrame(
+    data={
+        "id": [2, 3, 4, 4, 4],
+        "sales": [100, 200, 300, 500, 700],
+        "created": [
+            datetime.now(),
+            datetime.now() - timedelta(minutes=1),
+            datetime.now() - timedelta(minutes=2),
+            datetime.now() - timedelta(minutes=3),
+            datetime.now() - timedelta(minutes=5),
+        ],
+    }
+)
 
 # this returns the rows with id = 2 + 3, as expected
 identical_records = pd.merge(df1, df2, on="id")
@@ -24,4 +38,14 @@ filtered_df1 = (
     df2.merge(df1, on="id", how="left", indicator=True)
     .query('_merge == "left_only"')
     .drop("_merge", axis=1)
+)
+
+
+# grab latest timestamp records from df2
+df2["created"] = pd.to_datetime(df2["created"])
+
+deduplicated_df = (
+    df2.sort_values(by=["id", "created"], ascending=[True, False])
+    .drop_duplicates(subset="id", keep="first")
+    .query("created > '2023-08-29T16:19:47.008'")
 )
