580a750f7d3dce24f11aa872273135679ba0b465
jyablonski9 <jyablonski9@gmail.com>
2023-08-29 13:32:35 -0500

    add

--- generic_python/pandas_mismatch_joins.py
+++ generic_python/pandas_mismatch_joins.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
+# existing data
 df1 = pd.DataFrame(data={"id": [1, 2, 3]})
-df2 = pd.DataFrame(data={"id": [2, 3, 4]})
+
+# values we want to add in if not already present
+df2 = pd.DataFrame(data={"id": [2, 3, 4], "sales": [100, 200, 300]})
 
 # this returns the rows with id = 2 + 3, as expected
 identical_records = pd.merge(df1, df2, on="id")
@@ -15,3 +18,10 @@ mismatch_records = (
     df3.query('_merge != "both"').copy().rename(columns={"_merge": "origin"})
 )
 mismatch_records["origin"] = mismatch_records["origin"].map(indicator_label)
+
+# Merge DataFrames and filter out rows with duplicate IDs in df1
+filtered_df1 = (
+    df2.merge(df1, on="id", how="left", indicator=True)
+    .query('_merge == "left_only"')
+    .drop("_merge", axis=1)
+)
