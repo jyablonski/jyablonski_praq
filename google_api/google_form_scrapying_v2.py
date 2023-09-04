8bac5e854d9023b84960415bd2669ccbea5e0de1
jyablonski9 <jyablonski9@gmail.com>
2023-09-04 13:37:28 -0500

    add

--- google_api/google_form_scrapying_v2.py
+++ google_api/google_form_scrapying_v2.py
@@ -1,6 +1,14 @@
 import gspread
 import pandas as pd
 
+from datetime import datetime, timedelta
+import uuid
+
+import mysql.connector
+import pandas as pd
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine.base import Connection
+
 # https://docs.gspread.org/en/latest/oauth2.html
 # create new project from google developer console
 # create new service account from `APIs & Services -> Credentials -> Service Account`
@@ -8,7 +16,7 @@ import pandas as pd
 # share the google sheet with the `client_email` provided in the service account json creds
 # enable google sheets api in the project
 
-url = "https://docs.google.com/spreadsheets/d/zzz"
+url = "https://docs.google.com/spreadsheets/d/1TMN5L0cfxQvKQdAHtXJrDbNepBKOJJCNQkC0bq6DdMA"
 
 # creds = {
 #   "type": "service_account",
@@ -33,4 +41,56 @@ sh = gc.open_by_url(url)
 
 worksheet = sh.get_worksheet(0)
 
-df = pd.DataFrame(worksheet.get_all_records())
+new_records = pd.DataFrame(worksheet.get_all_records())
+new_records = new_records.rename(
+    columns={
+        "Timestamp": "parkResponseDate",
+        "What's your favorite color": "parkComment",
+    }
+)
+new_records = new_records.drop("Select an Option", axis=1)
+new_records["parkResponseDate"] = pd.to_datetime(new_records["parkResponseDate"])
+
+# dump into mysql
+connection_string = "mysql+mysqlconnector://mysqluser:mysqlpw@127.0.0.1:3306/demo"
+mysql_engine = create_engine(connection_string, echo=True)
+connection = mysql_engine.connect()
+table = "movies_test"
+
+# this also works
+# df = pd.read_sql_query(sql = "select * from demo.movies;", con=mysql_engine)
+df = pd.read_sql_query(sql="select * from demo.movies;", con=connection)
+df2 = df.query("genres == 'Western'")
+
+# insert new records
+results = df2.to_sql(
+    name="movies_test", con=connection, if_exists="replace", index=False
+)
+print(f"{results} records inserted into {table}")
+
+reviews_json = {
+    "postStaySurveyId": [1, 2, 3],
+    "machineReviewStatus": ["REJECTED", "NEEDS_HUMAN_REVIEW", "APPROVED"],
+    "humanReviewStatus": [None, None, None],
+    "campspotResponse": [None, None, None],
+    "campspotInternalComment": ["", "", ""],
+    "parkResponse": [None, None, None],
+    "parkComment": ["", "", ""],
+    "viewable": [None, None, None],
+    "language": ["eng", "eng", "eng"],
+}
+
+reviews = pd.DataFrame(data=reviews_json)
+reviews["uuid"] = [str(uuid.uuid4()) for _ in range(len(reviews))]
+
+reviews.to_sql(
+    name="PostStaySurveyModeration", con=connection, if_exists="replace", index=False
+)
+
+sql_update = """
+update PostStaySurveyModeration
+set parkComment = :parkComment, parkResponseDate = :parkResponseDate, modified = now()
+where uuid = :uuid
+"""
+params = new_records.to_dict("records")
+connection.execute(text(sql_update), params)
