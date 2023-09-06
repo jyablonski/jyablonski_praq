cf9d62838a6c8a977e8a4db2c1f60132a3208511
jyablonski9 <jyablonski9@gmail.com>
2023-09-05 20:53:05 -0500

    z

--- generic_python/python_if_else.py
+++ generic_python/python_if_else.py
@@ -8,6 +8,13 @@ df = pd.DataFrame(
         "id": [1, 2, 3, 4, 5],
         "sales": [100, 200, 300, 0, 700],
         "is_valid": [True, True, False, False, True],
+        "review_text": [
+            "hellooo world this is a long striiiiiing whssich should have 8 partszzzzz",
+            "hii",
+            "hii v2",
+            "x.",
+            "y me",
+        ],
     }
 )
 
@@ -18,3 +25,44 @@ df["viewable_status"] = np.where(
     "Approved",
     np.where(df["sales"] == 0, "Rejected", "Needs Review"),
 )
+
+len_limit = 10
+
+for index, row in df.iterrows():
+    total_len_review_text = len(row["review_text"])
+
+    # split the text up based on the len limit
+    if total_len_review_text > len_limit:
+        chunks = [
+            row["review_text"][i : i + len_limit]
+            for i in range(0, total_len_review_text, len_limit)
+        ]
+        print(f"{len(chunks)} total chunks")
+
+        review_recommended = []
+        category_1 = []
+        category_2 = []
+        category_3 = []
+
+        for review_chunk in chunks:
+            # do some processing as usual
+
+            # append
+            review_recommended.append(False)
+            category_1.append(1)
+            category_2.append(1)
+            category_3.append(1)
+            # finish the processing, then aggregate the results together and take averages
+
+        if True in review_recommended:
+            df["review_recommended"] = True
+        else:
+            df["review_recommended"] = False
+
+        df["category1"] = sum(category_1) / len(category_1)
+        df["category2"] = sum(category_2) / len(category_2)
+        df["category3"] = sum(category_3) / len(category_3)
+    else:
+        print(f"less than {len_limit}")
+
+print(df)
