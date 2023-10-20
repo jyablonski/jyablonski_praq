b5909b3d1ca7abbb3c4e4a357fcba3713eb0df67
jyablonski9 <jyablonski9@gmail.com>
2023-10-20 18:05:22 -0500

    add

--- sql/rds_parquet_table_backups.py
+++ sql/rds_parquet_table_backups.py
@@ -2,69 +2,23 @@ from datetime import datetime
 import os
 
 import pandas as pd
-from sqlalchemy import exc, create_engine
+from jyablonski_common_modules.sql import sql_connection, write_to_sql
 
 # took like a minute and a half as of 2023-02-04
 date = datetime.now().date()
 print(f"Starting Export at {datetime.now()}")
 
 
-def write_to_sql(con, table_name: str, df: pd.DataFrame, table_type: str) -> None:
-    """
-    SQL Table function to write a pandas data frame in aws_dfname_source format
-    Args:
-        con (SQL Connection): The connection to the SQL DB.
-        table_name (str): The Table name to write to SQL as.
-        df (DataFrame): The Pandas DataFrame to store in SQL
-        table_type (str): Whether the table should replace or append to an existing SQL Table under that name
-    Returns:
-        Writes the Pandas DataFrame to a Table in Snowflake in the {nba_source} Schema we connected to.
-    """
-    try:
-        if len(df) == 0:
-            print(f"{table_name} is empty, not writing to SQL")
-        else:
-            df.to_sql(
-                con=con,
-                name=f"aws_{table_name}_source",
-                index=False,
-                if_exists=table_type,
-            )
-            print(
-                f"Writing {len(df)} {table_name} rows to aws_{table_name}_source to SQL"
-            )
-    except BaseException as error:
-        print(f"SQL Write Script Failed, {error}")
+engine = sql_connection(
+    database=os.environ.get("RDS_DB"),
+    schema="nba_source",
+    user=os.environ.get("RDS_USER"),
+    pw=os.environ.get("RDS_PW"),
+    host=os.environ.get("IP"),
+)
 
 
-def sql_connection(rds_schema: str):
-    """
-    SQL Connection function connecting to my postgres db with schema = nba_source where initial data in ELT lands.
-    Args:
-        rds_schema (str): The Schema in the DB to connect to.
-    Returns:
-        SQL Connection variable to a specified schema in my PostgreSQL DB
-    """
-    RDS_USER = os.environ.get("RDS_USER")
-    RDS_PW = os.environ.get("RDS_PW")
-    RDS_IP = os.environ.get("IP")
-    RDS_DB = os.environ.get("RDS_DB")
-    try:
-        connection = create_engine(
-            f"postgresql+psycopg2://{RDS_USER}:{RDS_PW}@{RDS_IP}:5432/{RDS_DB}",
-            connect_args={"options": f"-csearch_path={rds_schema}"},
-            # defining schema to connect to
-            echo=False,
-        )
-        print(f"SQL Connection to schema: {rds_schema} Successful")
-        return connection
-    except exc.SQLAlchemyError as e:
-        return e
-
-
-conn = sql_connection(rds_schema="nba_source")
-
-with conn.connect() as connection:
+with engine.connect() as connection:
     df = pd.read_sql(
         "SELECT * FROM information_schema.tables where table_schema = 'nba_source';",
         connection,
