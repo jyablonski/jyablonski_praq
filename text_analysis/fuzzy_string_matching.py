ada399117a3542ef961e1098a5ad90c2c870ef30
jyablonski9 <jyablonski9@gmail.com>
2024-07-18 20:53:04 -0500

    add fuzzy match praqa

--- text_analysis/fuzzy_string_matching.py
+++ text_analysis/fuzzy_string_matching.py
@@ -1,32 +1,102 @@
 from thefuzz import fuzz
-import pandas as pd
+import polars as pl
 
-# package seems okay.  depending on the type of string matching you're trying to do there are
-# multiple different functions that you can use to get a more accurate match.
-# it's still not going to be 100% though; if ever using this in an actual production setting
-# you'd need some form of manual QA to comb through this and make sure the matching is doing its job correctly.
-# https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
+# dummy example i built using polars and thefuzz package. should split this out into functions
+# so they can be independently tested.
 
-names_df = pd.DataFrame(
-    data={
-        "id": [
-            1,
-            2,
-            3,
-            4,
-        ],
-        "name_1": ["Jacob Yablonski", "Terry Lewis", "Marie Obal", "Erin Booster"],
-        "name_2": ["Jacob Yablonksi", "Terry Lewsi", "Mary Oble", "Aaron Booster"],
-    }
+# example data - say we're joining people names + emails on 2 different
+# data sources
+data = {
+    "id": [1, 2, 3, 4, 5, 6],
+    "name_1": [
+        "Jacob Yablonski",
+        "Terry Lewis",
+        "Marie Obal",
+        "Erin Booster",
+        "James Harrison",
+        "Erik Lave",
+    ],
+    "email_1": [
+        "jacob.yablonski@example.com",
+        "terry.lewis@example.com",
+        "marie.obal@example.com",
+        "erin.booster@example.com",
+        "james.harrison@example.com",
+        "erik.lave@example.com",
+    ],
+    "birthdate_1": [
+        "1985-05-15",
+        "1990-06-20",
+        "1988-03-22",
+        "1992-07-10",
+        "1983-11-02",
+        "1995-01-08",
+    ],
+    "name_2": [
+        "Jacob Yablonksi",
+        "Terry Lewsi",
+        "Mary Oble",
+        "Aaron Booster",
+        "James Harrison II",
+        "Erik Lave Jr.",
+    ],
+    "email_2": [
+        "jacob.yablonski@example.com",
+        "terry.lewis@example.com",
+        "mary.oble@example.com",
+        "aaron.booster@example.com",
+        "james.harrison@example.com",
+        "erik.lave@example.com",
+    ],
+    "birthdate_2": [
+        "1985-05-15",
+        "1990-06-21",
+        "1988-09-23",
+        "1992-07-10",
+        "1983-11-03",
+        "1995-01-09",
+    ],
+}
+
+
+match_threshold_pct = 0.75
+
+fuzzy_df = pl.DataFrame(data=data)
+
+# if we get an email match, then use that
+email_matches = fuzzy_df.filter(pl.col("email_1") == pl.col("email_2")).with_columns(
+    is_match=pl.lit(1)
 )
 
-# iterate through the rows in the dataframe
-names_df["str_match_score"] = [
-    fuzz.ratio(x, y) for x, y in zip(names_df["name_1"], names_df["name_2"])
-]
+# if we dont get an email match, then let's fuzzy match the name and
+# take birthdate into account
+# this is slow because we have to use python to map this custom fuzz.ratio function
+# to each row in the dataframe, but gotta do what u gotta do
+mismatches = (
+    fuzzy_df.join(other=email_matches, on=pl.col("id"), how="anti")
+    .with_columns(
+        pl.struct(["name_1", "name_2"])
+        .map_elements(
+            lambda x: fuzz.ratio(x["name_1"], x["name_2"]), return_dtype=pl.Int64
+        )
+        .alias("str_match_score")
+    )
+    .with_columns(
+        is_match=pl.when(
+            pl.col("str_match_score") >= match_threshold_pct,
+            pl.col("birthdate_1") == pl.col("birthdate_2"),
+        )
+        .then(1)
+        .otherwise(0)
+    )
+)
 
-names_df["str_match_partial_score"] = [
-    fuzz.partial_ratio(x, y) for x, y in zip(names_df["name_1"], names_df["name_2"])
-]
-
-print(names_df)
+# return final df, with the appropriate matches
+combined_df = pl.concat(
+    [
+        email_matches,
+        mismatches.drop(
+            "str_match_score",
+        ),
+    ]
+)
