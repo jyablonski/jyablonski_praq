319c4090c5a6494cfeae2820baf80dc4116deef6
jyablonski9 <jyablonski9@gmail.com>
2025-02-05 19:44:24 -0800

    bleh

--- creative/boxes.py
+++ creative/boxes.py
@@ -1,4 +1,15 @@
+from itertools import cycle
+
 height = 12
 
 for i in range(0, 12):
     print(f" " * (height - i), "#" * i)
+
+valid_chars = ["#", "$"]
+char_cycle = cycle(valid_chars)
+
+for i in range(1, 26):
+    next_char = cycle(valid_chars)
+    expr = i * next_char if i % 2 == 0 else i * next_char
+
+    print(expr)
