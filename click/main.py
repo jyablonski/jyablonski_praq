c48a0f8d4b54d78b46ddb0ae34ebb4226199ea12
jyablonski9 <jyablonski9@gmail.com>
2025-04-06 12:08:45 -0700

    swap mfer

--- click/main.py
+++ click/main.py
@@ -1,11 +1,22 @@
 import click
 
 
-# usage: `python click/main.py --s3-file s3://test-bucket/test-file.csv`
+# example usage: `python click/main.py --s3-file s3://test-bucket/test-file.csv`
 @click.command()
 @click.option("--s3-file", required=True, help="S3 Path to the input file")
 def generate_statement(s3_file: str) -> None:
-    click.echo(f"Generating Statement for {s3_file}")
+    success = True if "test" in s3_file else False
+
+    click.secho("📂 Starting Statement Generation...", fg="blue", bold=True)
+
+    for i in range(1, 6):
+        click.echo(f"  ➜ Running Statement {i}")
+
+    if success:
+        click.secho("✅ Statement successfully generated!", fg="green", bold=True)
+    else:
+        click.secho("❌ Failed to generate statement.", fg="red", bold=True)
+
     return None
 
 
