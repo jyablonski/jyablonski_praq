448199f5f3ae16e3acee449d9b6ca2b5cba1ff00
jyablonski9 <jyablonski9@gmail.com>
2025-06-22 15:27:27 -0700

    add v2

--- leetcode/134_gas_station.py
+++ leetcode/134_gas_station.py
@@ -10,32 +10,29 @@
 # 'direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique.
 
 
+# use greedy algorithm because if a segment from station A to B makes you run out of gas,
+# no station between A and B can be a valid start and we can continue to the next one
 def solution(gas: list[int], cost: list[int]) -> int:
-    # initialize some variables to figure out if we can make an entire loop
-    total_gas = 0
-    total_cost = 0
-    current_tank = 0
-    start_index = 0
-
-    # iterate through from 0 - len(gas)
-    for i in range(len(gas)):
-        print(f"on index {i}")
-        total_gas += gas[i]
-        total_cost += cost[i]
-        current_tank += gas[i] - cost[i]
-
-        # this checks if we can make it to the i + 1 station or not
-        # if not, then we cant start at the index we're on. so add + 1
-        # to it and use the next one and reset current_tank back to 0
-        if current_tank < 0:
-            print(f"current_tank {current_tank} is less than 0")
-            start_index = i + 1
-            current_tank = 0
-
-    if total_gas < total_cost:
+    # if there isnt enough gas along the route, then return -1
+    # if there is enough gas, then we're guaranteed to have a valid solution somewhere
+    if sum(gas) < sum(cost):
         return -1
-    else:
-        return start_index
+
+    start = 0
+    fuel = 0
+
+    # iterate through the gas list
+    for i in range(len(gas)):
+        # whenever we don't have enough gas to reach the next station, we move our
+        # starting gas station to the next station and reset our gas tank.
+        if fuel + gas[i] - cost[i] < 0:
+            start = i + 1
+            fuel = 0
+        else:
+            # otherwise we have enough gas, so we add the remainer we'll have to our fuel
+            fuel += gas[i] - cost[i]
+
+    return start
 
 
 gas1 = [1, 2, 3, 4, 5]
