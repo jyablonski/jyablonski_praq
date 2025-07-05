a2ccbd1bf7ca9388e23b4082db4e5fd37ceafb5d
jacob <jyablonski9@gmail.com>
2025-07-05 10:38:47 -0700

    add sync test (#12)

--- leetcode/169_majority_element.py
+++ leetcode/169_majority_element.py
@@ -11,3 +11,35 @@ nums3 = [1, 1, 1, 2, 3, 4, 5, 6, 7]
 solution(nums)
 solution(nums2)
 solution(nums3)
+
+
+# vanilla way of doing it
+def majorityElement(self, nums: list[int]) -> int:
+    counts = {}
+
+    for value in nums:
+        if value not in counts:
+            counts[value] = 1
+        else:
+            counts[value] += 1
+
+    counts_sorted = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+
+    return counts_sorted[0][0]
+
+
+# other solution w/ no dictionary
+def majorityElement(self, nums: list[int]) -> int:
+    ans = -1
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            ans = num
+
+        if ans == num:
+            count += 1
+        else:
+            count -= 1
+
+    return count
